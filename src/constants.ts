6a51cc3656a47d8f0c6772ec1007bdf9ab42cc54
APukas <arminaspukas11@gmail.com>
2020-10-09 14:04:51 +0300

    Apollo and express server setup.

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,4 @@
 export const PROD = process.env.NODE_ENV === "production";
 export const USER = process.env.USER;
 export const PASSWORD = process.env.PASSWORD;
+export const PORT = process.env.PORT;
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,28 @@
 import { MikroORM } from "@mikro-orm/core";
+import { ApolloServer } from "apollo-server-express";
+import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 
 import mikroOrmConfig from "./mikro-orm.config";
+import { PORT } from "./constants";
+import { buildSchema } from "type-graphql";
+import { PostResolver } from "./resolvers/Post";
 
 const main = async () => {
   const orm = await MikroORM.init(mikroOrmConfig);
   await orm.getMigrator().up();
+
+  const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({ resolvers: [PostResolver], validate: false }),
+  });
+
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port: ${PORT}`);
+  });
 };
-main().catch((e) => console.log(e));
+main().catch((e) => console.error(e));
